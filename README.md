b4d5e35416dd9559ba9f313f4438abce25945b99
Juan Luque <juand1121@gmail.com>
2022-01-02 22:52:50 -0500

    Basic functionality working.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Obsidian share connected component
+This hacky code snippet will take obsidian notes and create a new vault containing said notes and any notes directly and indirectly linked to by them. In this manner we can share a note (or a few notes) with someone while also sharing any note that will be referenced by these notes.
+
+### Instructions
+1. Copy `main.py` to the root of your vault.
+2. Open the vault in your terminal.
+3. To share the two notes `my thoughts.md` and `recipes.md` run 
+```
+python3 main.py "my thoughts.md" "recipes.md"
+```
+4. Done! There should be a new vault titled `my_new_vault_dir` in the same directory as the original vault.
---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+import re
+import os
+import shutil
+
+
+VAULT_DIR = '../my_new_vault_dir'
+
+# this dict maps note names to their respective path, 
+notes_map = {}
+def populate_notes_map():
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            notes_map[name] = os.path.join(root, name)
+
+def get_connected_components(seeds):
+    populate_notes_map()
+    seen = set()
+    stack = seeds
+    while stack:
+        fn = stack.pop()
+        outgoing_links = get_links(fn)
+        seen.add(fn)
+        seen.update(outgoing_links)
+    return list(seen)
+
+
+def get_links(filename: str):
+    if not os.path.isfile(filename):
+        return []
+    with open(filename, 'r') as f:
+        note = f.read()
+    pattern = r'\[\[([^\[\|]+)\|?[^\[\|]*\]\]'
+    matches = re.findall(pattern, note)
+    matches = [notes_map[m.strip() + '.md'] for m in matches]
+    return matches
+
+
+def make_new_vault(target_notes):
+    if not os.path.exists(VAULT_DIR):
+        os.mkdir(VAULT_DIR)
+    for note in target_notes:
+        note_dir = os.path.dirname(note)
+        note_dir_in_new_vault = os.path.join(VAULT_DIR, note_dir)
+        if note_dir != '' and not os.path.exists(note_dir_in_new_vault):
+            os.mkdir(note_dir_in_new_vault)
+        shutil.copy2(note, os.path.join(VAULT_DIR, note))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    target_notes = get_connected_components(sys.argv[1:])
+    print(f'Copying new notes: {target_notes}')
+    print(f'Making new vault at: {VAULT_DIR}/')
+    make_new_vault(target_notes)
